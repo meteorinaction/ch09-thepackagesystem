c7767fdd3b5f3d96a106b1d49d63755a0be6ef3a
Stephan Hochhaus <stephan@yauh.de>
2015-04-26 13:44:19 +0200

    prepare client to make call to gravatar

--- app/app.js
+++ app/app.js
@@ -1,5 +1,18 @@
 if (Meteor.isClient) {
+  Session.setDefault('gravatarUrl', false);
 
+  Template.gravatar.helpers({
+    gravatarUrl: function () {
+      return Session.get('gravatarUrl');
+    }
+  });
+
+  Template.gravatar.events({
+    'click button': function (evt, tpl) {
+      var email = tpl.find('input#email').value;
+      console.log('I\'d like to see the gravatar image for ' + email);
+    }
+  });
 }
 
 if (Meteor.isServer) {
