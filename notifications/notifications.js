5ae8d8c1b75cb8304ba05647718fd3da8a501bec
Stephan Hochhaus <stephan@yauh.de>
2015-04-26 14:18:57 +0200

    Listing 9.7 Exposing package functionality via the Notifications global

--- notifications/notifications.js
+++ notifications/notifications.js
@@ -1,3 +1,30 @@
+Notification = {
+  setError: function (text) {
+    Session.set('notify', {
+      type: 'error',
+      text: text,
+      buttonText: 'Oh, no.'
+    });
+  },
+  setWarning: function (text) {
+    Session.set('notify', {
+      type: 'warning',
+      text: text,
+      buttonText: 'Good to know...'
+    });
+  },
+  setSuccess: function (text) {
+    Session.set('notify', {
+      type: 'success',
+      text: text,
+      buttonText: 'Cool!'
+    });
+  },
+  clear: function () {
+    Session.set('notify', '');
+  }
+};
+
 Template.notificationArea.helpers({
   notification: function () {
     return Session.get('notify');
