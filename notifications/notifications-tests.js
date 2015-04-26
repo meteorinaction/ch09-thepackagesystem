b90c87ca5b9e6e09bdd9a4ebc2b6fadec99065e1
Stephan Hochhaus <stephan@yauh.de>
2015-04-26 14:50:50 +0200

    more, structured tests added

--- notifications/notifications-tests.js
+++ notifications/notifications-tests.js
@@ -1,6 +1,28 @@
-Tinytest.add('setError', function (test) {
+Tinytest.add('Msg - Error - setError', function (test) {
   var msgText = 'An error message';
   Notification.setError(msgText);
   test.equal(Session.get('notify').text, msgText);
   test.equal(Session.get('notify').type, 'error');
+});
+
+Tinytest.add('Msg - Success - setSuccess', function (test) {
+  var msgText = 'A success message';
+  Notification.setSuccess(msgText);
+  test.equal(Session.get('notify').text, msgText);
+  test.equal(Session.get('notify').type, 'success');
+});
+
+Tinytest.add('Msg - Warning - setWarning', function (test) {
+  var msgText = 'A warning message';
+  Notification.setWarning(msgText);
+  test.equal(Session.get('notify').text, msgText);
+  test.equal(Session.get('notify').type, 'warning');
+});
+
+Tinytest.add('Msg - clear', function (test) {
+  var msgText = 'A warning message';
+  Notification.setWarning(msgText);
+  Notification.clear();
+  test.equal(Session.get('notify').text, undefined);
+  test.equal(Session.get('notify').type, undefined);
 });
